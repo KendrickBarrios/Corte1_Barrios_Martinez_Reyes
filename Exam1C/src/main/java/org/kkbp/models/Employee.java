43384a4016c56b2776bf2baa4c66c778910f499a
KendrickBarrios <kkbarrios@uamv.edu.ni>
2024-09-25 16:17:55 -0600

    Se agregan las clases Employee y Deliveryman

---
+++ Exam1C/src/main/java/org/kkbp/models/Employee.java
@@ -0,0 +1,72 @@
+package org.kkbp.models;
+
+public class Employee {
+    private String id;
+    private String names;
+    private String lastNames;
+    private int age;
+    private double base_salary;
+    static int INCENTIVE = 300;
+
+    public Employee(){
+
+    }
+
+    public Employee(String id, String names, String lastNames, int age, double base_salary) {
+
+        this.id = id;
+        this.names = names;
+        this.lastNames = lastNames;
+        this.age = age;
+        this.base_salary = base_salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getLastNames() {
+        return lastNames;
+    }
+
+    public void setLastNames(String lastNames) {
+        this.lastNames = lastNames;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getBase_salary() {
+        return base_salary;
+    }
+
+    public void setBase_salary(double base_salary) {
+        this.base_salary = base_salary;
+    }
+
+    @Override
+    public String toString() {
+        return  "{id='" + id + '\'' +
+                ", names='" + names + '\'' +
+                ", lastNames='" + lastNames + '\'' +
+                ", age=" + age +
+                ", base_salary=" + base_salary;
+    }
+}
